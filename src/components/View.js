32c8df5e8397d1795a7ed0ebf3bd4c8a2372c4a2
Matheus <matheus@respondeai.com.br>
2021-10-27 09:57:30 -0300

    preparando o review

--- src/components/View.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export default styled.div`
-    padding-top: 20px;
-    margin: 10px calc((100% / 2) - 160px);
-`;
--- src/components/ViewAuthentication.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export default styled.main`
-	height: 100vh;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-`;
